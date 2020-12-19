664d1b091dd6402a338f189d643db7c8eda5a375
Abraham <qianyonglian@outlook.com>
2020-12-19 11:21:57 +0800

    Initial Commit

---
+++ IOSXcodeApp/IOSXcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSXcodeAppApp.swift
+//  IOSXcodeApp
+//
+//  Created by AbrahamQian on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct IOSXcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
