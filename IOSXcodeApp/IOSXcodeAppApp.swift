d5c026af123e3c3317e062fd54c19514f0c25f0a
Abraham <qianyonglian@outlook.com>
2020-12-19 12:27:10 +0800

    tweak2

--- IOSXcodeApp/IOSXcodeAppApp.swift
+++ IOSXcodeApp/IOSXcodeAppApp.swift
@@ -6,9 +6,9 @@
 //
 
 import SwiftUI
-//import AppCenter
-//import AppCenterAnalytics
-//import AppCenterCrashes
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct IOSXcodeAppApp: App {
@@ -23,10 +23,10 @@ struct IOSXcodeAppApp: App {
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        AppCenter.start(withAppSecret: "5adbf1d7-33ea-4d5c-89d1-d97d58a9b40d", services:[
-//          Analytics.self,
-//          Crashes.self
-//        ])
+        AppCenter.start(withAppSecret: "5adbf1d7-33ea-4d5c-89d1-d97d58a9b40d", services:[
+          Analytics.self,
+          Crashes.self
+        ])
         
         print("Hello,I am here")
         return true
